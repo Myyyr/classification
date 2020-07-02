32242e07538d251d297b30ebdf4f26a5c61f72dd
MYR <loic.themyr@gmail.com>
2020-07-02 10:40:18 +0200

    tmp

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-import pydicom
+# import pydicom
 
 import torch
 import torch.nn.functional as F
