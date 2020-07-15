83ee49ce7b1207f2eff905ae7cdce2475f7f962d
MYR <loic.themyr@gmail.com>
2020-07-15 15:54:41 +0200

    tmp

--- configs/config_revnet18.py
+++ configs/config_revnet18.py
@@ -1,6 +1,7 @@
 import models.RevNet as RevNet
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = '2' 
+import scheduler
 
 BATCH_SIZE = 128
 N_WORKERS = 2
@@ -14,6 +15,9 @@ MOMENTUM = 0.9#0.9
 
 
 LRS = [1,0.1, 0.01, 0.001, 0.0001]
+
+def SCHEDULER(optimizer):
+	return scheduler.MoreSimpleScheduler(optimizer, LRS)
 
 MODEL = RevNet.RevNet18()
 
