139fb849c4abd743c4f80b494601afe835c80267
MYR <loic.themyr@gmail.com>
2020-08-31 10:11:37 +0200

    cifar100 model + config

--- models/ResNet.py
+++ models/ResNet.py
@@ -108,11 +108,11 @@ def ResNet18(num_classes = 10):
     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes = num_classes)
 
 
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+def ResNet34(num_classes = 10):
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes = num_classes)
 
-def ResNet48():
-    return ResNet(BasicBlock, [5, 6, 7, 5])
+def ResNet48(num_classes = 10):
+    return ResNet(BasicBlock, [5, 6, 7, 5], num_classes = num_classes)
 
 def ResNet50():
     return ResNet(Bottleneck, [3, 4, 6, 3])
