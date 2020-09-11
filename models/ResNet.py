6e6f074f7b72491e36f7ea8e54d131d20154f626
MYR <loic.themyr@gmail.com>
2020-09-11 15:21:55 +0200

    debug

--- models/ResNet.py
+++ models/ResNet.py
@@ -42,7 +42,7 @@ class SimpleBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
+        super(SimpleBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
